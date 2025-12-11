98bb031caf6265afdc5ae0a7002529cb4e0d1ce6
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-12-11 19:36:42 +0100

    update example with search_texts

--- examples/modes.rs
+++ examples/modes.rs
@@ -16,37 +16,74 @@ struct Args {
     /// #patterns
     #[clap(short)]
     p: usize,
+    /// #texts
+    #[clap(short)]
+    t: usize,
+
+    #[clap(long)]
+    pattern: bool,
+    #[clap(long)]
+    text: bool,
 }
 
 fn main() {
     env_logger::init();
 
-    let Args { n, m, k, p } = Args::parse();
+    let Args {
+        n,
+        m,
+        k,
+        p,
+        t,
+        pattern,
+        text,
+    } = Args::parse();
 
     let mut rng = rand::rng();
-    let text: Vec<u8> = (0..n).map(|_| b"ACGT"[rng.random_range(0..4)]).collect();
+    let texts: Vec<Vec<u8>> = (0..t)
+        .map(|_| (0..n).map(|_| b"ACGT"[rng.random_range(0..4)]).collect())
+        .collect();
+    let texts: Vec<&[u8]> = texts.iter().map(|t| t.as_slice()).collect();
     let patterns: Vec<Vec<u8>> = (0..p)
         .map(|_| (0..m).map(|_| b"ACGT"[rng.random_range(0..4)]).collect())
         .collect();
 
-    {
-        let start = std::time::Instant::now();
-        let mut cnt = 0;
+    if !pattern && !text {
         let mut searcher = Searcher::<Iupac>::new_rc();
-        for pattern in &patterns {
-            let matches = searcher.search(pattern, &text, k);
-            // eprintln!("matches: {matches:?}");
-            cnt += matches.len();
+        for _ in 0..2 {
+            let start = std::time::Instant::now();
+            let mut cnt = 0;
+            for pattern in &patterns {
+                for text in &texts {
+                    let matches = searcher.search(pattern, &text, k);
+                    cnt += matches.len();
+                }
+            }
+            eprintln!("Single    {cnt}: {:?}", start.elapsed());
         }
-        eprintln!("Single    {cnt}: {:?}", start.elapsed());
     }
-    {
-        let start = std::time::Instant::now();
-        let mut cnt = 0;
+    if !pattern {
         let mut searcher = Searcher::<Iupac>::new_rc();
-        let matches = searcher.search_patterns(&patterns, &text, k, None);
-        // eprintln!("matches: {matches:?}");
-        cnt += matches.len();
-        eprintln!("Patterns: {cnt}: {:?}", start.elapsed());
+        for _ in 0..2 {
+            let start = std::time::Instant::now();
+            let mut cnt = 0;
+            for pattern in &patterns {
+                let matches = searcher.search_texts(&pattern, &texts, k, None);
+                cnt += matches.len();
+            }
+            eprintln!("Text      {cnt}: {:?}", start.elapsed());
+        }
+    }
+    if !text {
+        let mut searcher = Searcher::<Iupac>::new_rc();
+        for _ in 0..2 {
+            let start = std::time::Instant::now();
+            let mut cnt = 0;
+            for text in &texts {
+                let matches = searcher.search_patterns(&patterns, &text, k, None);
+                cnt += matches.len();
+            }
+            eprintln!("Patterns: {cnt}: {:?}", start.elapsed());
+        }
     }
 }
