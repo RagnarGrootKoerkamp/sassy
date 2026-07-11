507df79ecf8ac5253e2ea26b800755ddf9c23e2f
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-07-11 10:43:03 +0200

    backend: drop unused macro argument

--- src/pattern_tiling/backend.rs
+++ src/pattern_tiling/backend.rs
@@ -1,3 +1,5 @@
+//! Implement our own wrapper trait around SIMD types
+
 use std::ops::{Add, BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, Shl, Shr, Sub};
 use wide::{u8x32, u16x16, u32x8, u64x4};
 
@@ -44,7 +46,7 @@ pub trait SimdBackend: Copy + 'static + Send + Sync + Default + std::fmt::Debug 
 }
 
 macro_rules! impl_wide_backend {
-    ($name:ident, $simd_ty:ty, $scalar:ty, $lanes:expr, $bits:expr, $pattern:ty) => {
+    ($name:ident, $simd_ty:ty, $scalar:ty, $lanes:expr, $bits:expr) => {
         #[derive(Clone, Copy, Debug, Default)]
         pub struct $name;
 
@@ -113,16 +115,16 @@ macro_rules! impl_wide_backend {
 }
 
 // Define Backends
-impl_wide_backend!(U64, u64x4, u64, 4, 64, [u8; 64]);
-impl_wide_backend!(U32, u32x8, u32, 8, 32, u8x32);
-impl_wide_backend!(U16, u16x16, u16, 16, 16, [u8; 16]);
-impl_wide_backend!(U8, u8x32, u8, 32, 8, [u8; 8]);
+impl_wide_backend!(U64, u64x4, u64, 4, 64);
+impl_wide_backend!(U32, u32x8, u32, 8, 32);
+impl_wide_backend!(U16, u16x16, u16, 16, 16);
+impl_wide_backend!(U8, u8x32, u8, 32, 8);
 
 #[cfg(all(target_arch = "x86_64", target_feature = "avx512f"))]
-impl_wide_backend!(U64_512, u64x8, u64, 8, 64, [u8; 64]);
+impl_wide_backend!(U64_512, u64x8, u64, 8, 64);
 #[cfg(all(target_arch = "x86_64", target_feature = "avx512f"))]
-impl_wide_backend!(U32_512, u32x16, u32, 16, 32, [u8; 32]);
+impl_wide_backend!(U32_512, u32x16, u32, 16, 32);
 #[cfg(all(target_arch = "x86_64", target_feature = "avx512bw"))]
-impl_wide_backend!(U16_512, u16x32, u16, 32, 16, [u8; 16]);
+impl_wide_backend!(U16_512, u16x32, u16, 32, 16);
 #[cfg(all(target_arch = "x86_64", target_feature = "avx512bw"))]
-impl_wide_backend!(U8_512, u8x64, u8, 64, 8, [u8; 8]);
+impl_wide_backend!(U8_512, u8x64, u8, 64, 8);
